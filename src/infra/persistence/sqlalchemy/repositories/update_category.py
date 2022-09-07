c5df4b3a9176cd23ecae5a70c62586d2ef64df2e
Joel Fragoso <joelfragoso85@icloud.com>
2022-09-07 14:46:34 -0300

    :hammer: refactor: modifica o parâmetro  para

--- src/infra/persistence/sqlalchemy/repositories/update_category.py
+++ src/infra/persistence/sqlalchemy/repositories/update_category.py
@@ -3,8 +3,8 @@ from src.infra.persistence.sqlalchemy.models import Category
 
 
 class UpdateCategoryRepository:
-    def handle(self, _id: str, name: str, parent_id: str):
-        category = Category.query.get(_id)
+    def handle(self, category_id: str, name: str, parent_id: str):
+        category = Category.query.get(category_id)
         category.name = name
         category.parent_id = parent_id
         db.session.add(category)
