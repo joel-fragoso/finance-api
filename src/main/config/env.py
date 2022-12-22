6d9ee7d70ae0478575229c5eadf8cc4afd3eb65c
Joel Fragoso <joelfragoso85@icloud.com>
2022-12-22 19:27:56 -0300

    chore: ajusta as variáveis de ambientes

--- src/main/config/env.py
+++ src/main/config/env.py
@@ -1,5 +1,7 @@
+from distutils.util import strtobool
 from os import environ, path
-from dotenv import load_dotenv
+
+from dotenv import load_dotenv  # type: ignore
 
 base_dir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(base_dir, "..", "..", "..", ".env"))
@@ -7,5 +9,7 @@ load_dotenv(path.join(base_dir, "..", "..", "..", ".env"))
 
 class Config:
     # Database
-    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = strtobool(
+        environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", "True")
+    )
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI", "")
